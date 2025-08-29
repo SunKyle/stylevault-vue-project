ffe6f7ed90cafa5b09ba39ad83d452bf38c2c73c
Kyle Sun <sunxiaokai0914@163.com>
2025-08-29 14:04:43 +0800

    style: format eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,22 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     parser: '@babel/eslint-parser',
     ecmaVersion: 2020,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   globals: {
     defineProps: 'readonly',
     defineEmits: 'readonly',
     defineExpose: 'readonly',
-    withDefaults: 'readonly'
+    withDefaults: 'readonly',
   },
   rules: {
     // 允许单词组件名
     'vue/multi-word-component-names': 'off',
     // 允许未使用变量（如需更严格可删除此行）
-    'no-unused-vars': 'off'
-  }
-}
+    'no-unused-vars': 'off',
+  },
+};
